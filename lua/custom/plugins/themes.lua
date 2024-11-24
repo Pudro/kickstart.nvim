24aeadac8375b136cad1038a46d491c28cbdf89c
Piotr Pustelnik <ptr.pustelnik@gmail.com>
2024-11-24 16:19:35 +0100

    add themes.lua

---
+++ lua/custom/plugins/themes.lua
@@ -0,0 +1,8 @@
+return {
+  { 'projekt0n/github-nvim-theme' },
+  { 'catppuccin/nvim' },
+  { 'eemed/sitruuna.vim' },
+  { 'rebelot/kanagawa.nvim' },
+  { 'tjdevries/colorbuddy.nvim' },
+  { 'Mofiqul/vscode.nvim' },
+}
